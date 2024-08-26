ae315f2ac7dbebe1f525bdee20bd278339daef0b
kevin17xz <kpaz83444@gmail.com>
2024-08-26 17:00:49 -0600

    si le hice unos cambios

---
+++ comprar.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['confirmar_compra'])) {
+
+    // Código para manejar la compra y actualizar la base de datos
+
+} else {
+
+    // Mostrar el formulario si no se ha enviado nada aún
+    ?>
+
+    <form method="post" action="">
+        <!-- Campos del formulario aquí -->
+    </form>
+
+    <?php
+
+}
+?>
