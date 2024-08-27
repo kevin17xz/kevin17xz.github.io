f108d7f227d52abda8e2849809cf32621a1d6b9c
kevin17xz <kpaz83444@gmail.com>
2024-08-27 14:42:17 -0600

    agregue este ajuste

--- confirmar_compra.php
+++ confirmar_compra.php
@@ -1,5 +1,5 @@
 <?php // Conectar a la base de datos
-$servername = "localhost"; $username = ""; $password = ""; $dbname = "tienda_ropa"; $conn =  new mysqli($this->servidor, $this->usuario_mysql, $this->clave_mysql, $this->basedatos_mysql);// Verificar la conexión 
+$servername = "localhost"; $username = "root"; $password = ""; $dbname = "tienda_ropa"; $conn =  new mysqli($this->servidor, $this->usuario_mysql, $this->clave_mysql, $this->basedatos_mysql);// Verificar la conexión 
 if ($conn->connect_error) { die("Error de conexión: " . $conn->connect_error); } // Obtener los datos de la compra desde la URL 
 $producto_id = $_GET['producto']; $precio = $_GET['precio']; $usuario_id = 1; // Reemplaza con el ID del usuario actual // Preparar la consulta SQL
 $sql = "INSERT INTO compras (producto_id, usuario_id, fecha_compra, total) VALUES (?, ?, NOW(), ?)"; $stmt = $conn->prepare($sql); $stmt->bind_param("iid", $producto_id, $usuario_id, $precio); if ($stmt->execute()) { echo "¡Registro Guardado! Gracias por su compra."; } else { echo "Error al guardar la compra: " . $stmt->error; } $stmt->close(); $conn->close(); ?>
