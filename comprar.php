fe6b4a3ac7a790bf1a151538f2408b34b2fc50e2
kevin17xz <kpaz83444@gmail.com>
2024-08-27 22:57:30 -0600

    hice un gran cambio

--- comprar.php
+++ comprar.php
@@ -2,7 +2,7 @@
 // Configuración de la base de datos
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "1234567";
 $dbname = "tienda_ropa";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $productoId = $_POST['productoId'];
     $cantidad = $_POST['cantidad'];
 
-    // Consulta el stock del producto y verifica si hay suficientes unidades disponibles para esa cantidad
+    // Consulta el producto en la base de datos
     $sql = "SELECT * FROM productos WHERE id=?";
     if ($stmt = $conn->prepare($sql)) {
         $stmt->bind_param("i", $productoId);
@@ -31,12 +31,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $sql = "INSERT INTO compras (nombre_producto, precio, cantidad, fecha_compra)
                         VALUES (?, ?, ?, NOW())";
                 if ($stmt = $conn->prepare($sql)) {
-                    $stmt->bind_param("dii", $row['nombre'], $row['precio'], $cantidad);
+                    $stmt->bind_param("sdi", $row['nombre'], $row['precio'], $cantidad);
                     $stmt->execute();
-                    $compraId = $conn->insert_id;
 
                     // Muestra un mensaje de éxito al usuario
-                    echo "<p>¡Los datos han sido guardados! ¡MUCHAS GRACIAS POR SU COMPRA!</p>";
+                    echo "<p>¡Datos guardados! ¡GRACIAS POR SU COMPRA!</p>";
                 } else {
                     die("Falló la consulta: " . $conn->error);
                 }
@@ -44,15 +43,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 // Muestra un mensaje si el producto no está disponible en la cantidad seleccionada
                 echo "<p>No tenemos suficientes unidades disponibles para esa cantidad.</p>";
             }
+        } else {
+            echo "<p>Producto no encontrado.</p>";
         }
     } else {
         die("Falló la consulta: " . $conn->error);
     }
 
-    // Cierra las sentencias preparadas y conexión a la base de datos
-    if ($stmt) {
-        $stmt->close();
-    }
+    // Cierra las sentencias preparadas y la conexión a la base de datos
+    $stmt->close();
     $conn->close();
 }
 ?>
